e94a202d71efb59657033e9cf2df350d7d31b5b6
bilder <514059172@163.com>
2020-12-19 20:42:35 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // env Specifying the defined global variables
+  env: {
+    es2021: true,
+    webextensions: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
